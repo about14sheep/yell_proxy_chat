813a4eab169afd2248023080df8a434d56316f2a
Austin Burger <aburger91@gmail.com>
2020-12-29 15:37:25 -0600

    adds join responce to yellbot

--- yellbot.py
+++ yellbot.py
@@ -8,5 +8,10 @@ sio.connect('http://127.0.0.1:5000', namespaces=['/chat'])
 
 @sio.on('connect', namespace='/chat')
 def on_connect():
-    print('I am connected')
+    print('yellbot has entered the chat')
     sio.emit('join', {'user_id': 1, 'location_id': 1}, namespace='/chat')
+
+
+@sio.on('join_response', namespace='/chat')
+def on_join_response(data):
+    print('Room joined, server returned: ', data)

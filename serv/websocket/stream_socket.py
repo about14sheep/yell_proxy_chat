44a7ddd62fbfe77f796010bfa6c2d82fe211fd81
Austin Burger <aburger91@gmail.com>
2021-01-18 09:18:22 -0600

    updates on_stream_yell

--- serv/websocket/stream_socket.py
+++ serv/websocket/stream_socket.py
@@ -4,14 +4,10 @@ from . import Namespace, emit, rw, authenticated_only
 class Stream_Socket(Namespace):
 
     def on_connect(self):
-        emit('stream_connect_response',
-             {'data': 'Stream Connected'})
+        emit('stream_connect_response', {'data': 'Stream Connected'})
 
     def on_stream_yell(self, data):
-        msg = {
-            'username': data['username'],
-            'text': data['text']
-        }
+        msg = {'username': data['username'], 'text': data['text']}
         if rw.check_can_chat(data['user_id'], data['totem_id']) == 1:
             emote = rw.user_collect_emote(data['user_id'], data['emote_id'])
             if emote:

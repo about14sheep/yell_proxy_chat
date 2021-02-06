971ea2bd44f0b4e91ae39ab30a7126b13384dd70
Austin Burger <aburger91@gmail.com>
2021-02-06 08:58:58 -0600

    adds styling for better testing and organization

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -5,6 +5,12 @@ import Totem from './totem'
 import Channel from './channel'
 import User from './user'
 
+const mainStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-evenly'
+}
+
 function Main() {
   const totems = useSelector(state => state.totemReducer.totems)
   const user = useSelector(state => state.authReducer.user)
@@ -12,10 +18,18 @@ function Main() {
 
   return (
     <>
-      <h1>welcome to yell</h1>
-      <User user={user} />
-      {totems ? totems.map(el => <Totem totem={el} userID={user.id} key={el.totem_id} />) : <h3>Loading Totems</h3>}
-      {aChannel ? <Channel channel={aChannel} user={user} /> : <h3>join a channel to chat!</h3>}
+      <div style={mainStyle}>
+        <div>
+          <h1>welcome to yell</h1>
+          <User user={user} />
+        </div>
+        <div>
+          {totems ? totems.map(el => <Totem totem={el} userID={user.id} key={el.totem_id} />) : <h3>Loading Totems</h3>}
+        </div>
+        <div>
+          {aChannel ? <Channel channel={aChannel} user={user} /> : <h3>join a channel to chat!</h3>}
+        </div>
+      </div>
     </>
   )
 }
